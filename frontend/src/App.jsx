21643431eb00a47e47603b2691838eb308ce0303
Oliver Becker <ob@obqo.de>
2024-01-11 14:20:52 +0100

    :technologist: Set document title and fav icon for stage/dev environments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import {useDocumentTitle, useFavicon} from "@uidotdev/usehooks";
 import ApiProvider from "./ApiProvider";
 import Content from "./Content";
 import LoginRegistration from "./LoginRegistration";
@@ -8,7 +9,7 @@ export default function App() {
         <ApiProvider>
             {apiKey => (
                 <div className="min-h-screen relative">
-                    <StageBanner/>
+                    <StageDevAppearance/>
                     <div className="container mx-auto p-3 sm:p-8 pb-12 min-w-[352px] text-stone-900">
                         {apiKey ? <Content/> : <LoginRegistration/>}
                     </div>
@@ -19,17 +20,21 @@ export default function App() {
     )
 }
 
-function StageBanner() {
-    let host = window.location.host;
-
+function StageDevAppearance() {
+    const host = window.location.host;
     if (host === "www.causalist.de") { // prod
-        return null
+        return null;
     }
+
+    const stage = host === "stage.causalist.de";
+
+    useDocumentTitle(stage ? "S | Causalist" : "D | Causalist");
+    useFavicon("/logo-dev.svg");
 
     return (
         <>
-            <div className="h-8 p-1 bg-amber-300 font-semibold text-center fixed left-0 top-0 right-0 z-50">
-                {host === "stage.causalist.de" ? "Stage" : "Dev"}
+            <div className="h-8 p-1 bg-amber-300 font-semibold text-center fixed left-0 top-0 right-0 z-50 w-full">
+                {stage ? "Stage" : "Dev"}
             </div>
             <div className="h-8"/>
         </>
