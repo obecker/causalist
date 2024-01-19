b7840543ba3f66aeaffa1e4acd5096753aa7427f
Oliver Becker <ob@obqo.de>
2024-01-19 18:27:08 +0100

    :bug: Fix generating OpenAPI spec

--- backend/src/main/kotlin/de/obqo/causalist/api/Api.kt
+++ backend/src/main/kotlin/de/obqo/causalist/api/Api.kt
@@ -101,7 +101,7 @@ object Spec {
         ImportType.NEW_CASES,
         listOf("123 O 45/23", "123 O 67/23", "123 O 12/24"),
         listOf("123 O 3/23"),
-        emptyList(),
+        listOf("123 O 4/23"),
         listOf("Unknown reference")
     )
 
