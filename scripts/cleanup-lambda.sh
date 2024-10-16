68f63656360f1b8823c438f09ec8b0fff63eb2c5
Oliver Becker <ob@obqo.de>
2024-10-16 12:23:20 +0200

    :arrow_up: Bump go-lambda-cleanup from 2.0.13 to 2.0.15

--- scripts/cleanup-lambda.sh
+++ scripts/cleanup-lambda.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # terraform doesn't delete old function versions automatically, see https://github.com/hashicorp/terraform-provider-aws/issues/17668
 # this docker image will do the cleanup, see https://github.com/karl-cardenas-coding/go-lambda-cleanup
-docker run --rm -e AWS_ACCESS_KEY_ID="$(aws configure get aws_access_key_id)" -e AWS_SECRET_ACCESS_KEY="$(aws configure get aws_secret_access_key)" ghcr.io/karl-cardenas-coding/go-lambda-cleanup:2.0.13 clean -r "$(aws configure get region)"
+docker run --rm -e AWS_ACCESS_KEY_ID="$(aws configure get aws_access_key_id)" -e AWS_SECRET_ACCESS_KEY="$(aws configure get aws_secret_access_key)" ghcr.io/karl-cardenas-coding/go-lambda-cleanup:v2.0.15 clean -r "$(aws configure get region)"
