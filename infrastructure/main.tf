3544ecc231bf58d5e630ae569ca09dcc6419f1f5
Oliver Becker <ob@obqo.de>
2025-09-21 20:46:41 +0200

    :bricks: Add missing local provider to terraform configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.14.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4.1"
+    }
   }
 
   required_version = ">= 1.6.6"
