12ea9b9eb88d3d7fd202dd74c0e2e85bcc7d3e94
Oliver Becker <ob@obqo.de>
2026-02-05 20:56:50 +0100

    :recycle: Use Kotlin's Base64 implementation

--- backend/src/main/kotlin/de/obqo/causalist/LangUtils.kt
+++ backend/src/main/kotlin/de/obqo/causalist/LangUtils.kt
@@ -1,10 +1,10 @@
 package de.obqo.causalist
 
-import java.util.Base64
 import kotlin.experimental.xor
+import kotlin.io.encoding.Base64
 
-fun ByteArray.toBase64(): String = Base64.getEncoder().encodeToString(this)
+fun ByteArray.toBase64(): String = Base64.encode(this)
 
-fun String.fromBase64(): ByteArray = Base64.getDecoder().decode(this)
+fun String.fromBase64(): ByteArray = Base64.decode(this)
 
 infix fun ByteArray.xor(other: ByteArray) = zip(other) { a, b -> a xor b }.toByteArray()
